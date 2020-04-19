a6279d900ec356735b7566c552b58f7d2d9f2911
vijay0405 <uvijaysimha@gmail.com>
2020-04-19 21:21:17 +0530

    udpatd icon colors for product tile

--- shop_app/lib/widgets/productItem.dart
+++ shop_app/lib/widgets/productItem.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/produtDetailScreen.dart';
 
 class productItem extends StatelessWidget {
-
   final String id;
   final String title;
   final String imageUrl;
-  
 
   productItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-          child: GridTile(
-        child: Image.network(imageUrl, fit: BoxFit.cover,),
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+          onTap: (){
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: id);
+          },
+        ),
         footer: GridTileBar(
           backgroundColor: Colors.black38,
-          leading: IconButton(icon: Icon(Icons.favorite), onPressed: (){}, color: Theme.of(context).accentColor,),
-          trailing: IconButton(icon: Icon(Icons.shopping_cart), onPressed: (){}, color: Theme.of(context).accentColor,), 
-          title: Text(title, textAlign: TextAlign.center, ),
-
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
