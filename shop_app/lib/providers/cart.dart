466b81bd9f2c232afeb32e0db562160578aff1bc
vijay0405 <uvijaysimha@gmail.com>
2020-04-20 06:42:53 +0530

    Remove item from cart on swipe dismissible

--- shop_app/lib/providers/cart.dart
+++ shop_app/lib/providers/cart.dart
@@ -49,4 +49,10 @@ class Cart with ChangeNotifier {
     }
     notifyListeners();
   }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
 }
--- shop_app/lib/screens/cartScreen.dart
+++ shop_app/lib/screens/cartScreen.dart
@@ -46,6 +46,7 @@ class CartScreen extends StatelessWidget {
               child: ListView.builder(
                   itemBuilder: (ctx, i) => CartItem(
                       cart.items.values.toList()[i].id,
+                      cart.items.keys.toList()[i],
                       cart.items.values.toList()[i].title,
                       cart.items.values.toList()[i].quantity,
                       cart.items.values.toList()[i].price),
