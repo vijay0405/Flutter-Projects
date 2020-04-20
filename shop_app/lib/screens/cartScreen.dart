79eea2e4d4a2a301e490559dee3d9629dd5485df
vijay0405 <uvijaysimha@gmail.com>
2020-04-20 07:18:59 +0530

    Added orders screen

--- shop_app/lib/screens/cartScreen.dart
+++ shop_app/lib/screens/cartScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart';
 import '../widgets/cartItem.dart';
 
 class CartScreen extends StatelessWidget {
@@ -33,7 +34,11 @@ class CartScreen extends StatelessWidget {
                       backgroundColor: Theme.of(context).primaryColor,
                     ),
                     FlatButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Provider.of<Orders>(context, listen: false).addOrder(
+                            cart.items.values.toList(), cart.totalAmount);
+                        cart.clearCart();
+                      },
                       child: Text('Order Now'),
                       textColor: Theme.of(context).primaryColor,
                     )
