f8358bd205fd7465878bf4f6460cbdce7595cf74
vijay0405 <uvijaysimha@gmail.com>
2020-04-22 21:47:22 +0530

    added pull to refresh on manage products screen

--- shop_app/lib/screens/userProductsScreen.dart
+++ shop_app/lib/screens/userProductsScreen.dart
@@ -9,7 +9,11 @@ import './editProduct.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
-  
+
+  Future<void> _refreshProducts(BuildContext ctx) async {
+    await Provider.of<Products>(ctx).fetchAndSetData();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -26,20 +30,22 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: productsData.items.length,
-          itemBuilder: (ctx, i) => Column(
-            children: <Widget>[
-              UserProductItem(
-                productsData.items[i].id,
-                productsData.items[i].title,
-                productsData.items[i].imageUrl,
-              ),
-              Divider()
-            ],
-          ) 
+      body: RefreshIndicator(
+        onRefresh: ()=>_refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+              itemCount: productsData.items.length,
+              itemBuilder: (ctx, i) => Column(
+                    children: <Widget>[
+                      UserProductItem(
+                        productsData.items[i].id,
+                        productsData.items[i].title,
+                        productsData.items[i].imageUrl,
+                      ),
+                      Divider()
+                    ],
+                  )),
         ),
       ),
     );
