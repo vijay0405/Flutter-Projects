81518170457cb531c96fbda774121075937739a7
vijay0405 <uvijaysimha@gmail.com>
2020-04-22 22:29:20 +0530

    udpate fav status

--- shop_app/lib/providers/product.dart
+++ shop_app/lib/providers/product.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier {
   final String id;
@@ -8,11 +10,31 @@ class Product with ChangeNotifier {
   final String imageUrl;
   bool isFavorite;
 
-  Product({@required this.id, @required this.description, @required this.imageUrl, this.isFavorite = false, @required this.price,
+  Product(
+      {@required this.id,
+      @required this.description,
+      @required this.imageUrl,
+      this.isFavorite = false,
+      @required this.price,
       @required this.title});
 
-  void toggleFavStatus() {
+  void _setFavValue(newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavStatus() async {
+    final oldStatus = isFavorite;
+    final url = "https://shop-flutter-fec3d.firebaseio.com/products/$id.json";
     isFavorite = !isFavorite;
     notifyListeners();
+    try {
+      final response = await http.patch(url, body: json.encode({'isFavorite': isFavorite}));
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (err) {
+      _setFavValue(oldStatus);
+    }
   }
 }
