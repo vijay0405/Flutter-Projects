02728e03d37a1378e8a339b595115cf1bfed9083
vijay0405 <uvijaysimha@gmail.com>
2020-04-24 22:40:06 +0530

    added sqlite with insert for new place

--- my_places/lib/providers/places.dart
+++ my_places/lib/providers/places.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import '../models/place.dart';
+import '../helpers/dbHelper.dart';
 
 class Places with ChangeNotifier {
   List<Place> _items = [];
@@ -15,7 +16,12 @@ class Places with ChangeNotifier {
         image: pickedImage,
         title: title,
         location: null);
-        _items.add(newPlace);
-        notifyListeners();
+    _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
   }
 }
