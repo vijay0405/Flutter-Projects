0d94d2f43fad8b5f6e167a98f5ee11d7694b9efc
vijay0405 <uvijaysimha@gmail.com>
2020-04-28 19:17:01 +0530

    added change notifiter with consumer and appDrawer to home screen

--- expense_track/lib/main.dart
+++ expense_track/lib/main.dart
@@ -2,9 +2,13 @@ import 'package:expense_track/screens/authScreen.dart';
 import 'package:expense_track/widgets/newTransaction.dart';
 import 'package:expense_track/widgets/transactionList.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'widgets/transactionList.dart';
 import 'package:flutter/material.dart';
 import 'models/transaction.dart';
+import 'screens/splashScreen.dart';
+import 'widgets/appDrawer.dart';
+import 'providers/auth.dart';
 import 'widgets/chart.dart';
 
 void main() {
@@ -16,23 +20,36 @@ void main() {
 class Expense extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Track Expense',
-      home: AuthScreen(),
-      theme: ThemeData(
-          primarySwatch: Colors.teal,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Quicksand',
-          textTheme: ThemeData.light().textTheme.copyWith(
-                title: TextStyle(
-                    fontFamily: 'Quicksand',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18),
-                button: TextStyle(color: Colors.white),
-              ),
-          appBarTheme: AppBarTheme(
+    return ChangeNotifierProvider.value(
+      value: Auth(),
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+          title: 'Track Expense',
+          home: auth.isAuth
+              ? MyHomePage()
+              : FutureBuilder(
+                  future: auth.tryAutoLogin(),
+                  builder: (ctx, authResultSnapshot) =>
+                      authResultSnapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? SplashScreen()
+                          : AuthScreen()),
+          theme: ThemeData(
+              primarySwatch: Colors.teal,
+              accentColor: Colors.deepOrange,
+              fontFamily: 'Quicksand',
               textTheme: ThemeData.light().textTheme.copyWith(
-                  title: TextStyle(fontFamily: 'OpenSans', fontSize: 20)))),
+                    title: TextStyle(
+                        fontFamily: 'Quicksand',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                    button: TextStyle(color: Colors.white),
+                  ),
+              appBarTheme: AppBarTheme(
+                  textTheme: ThemeData.light().textTheme.copyWith(
+                      title: TextStyle(fontFamily: 'OpenSans', fontSize: 20)))),
+        ),
+      ),
     );
   }
 }
@@ -86,20 +103,20 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildLandscapeContent() {
-    return  Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text("Show Chart"),
-                Switch(
-                  value: _showChart,
-                  onChanged: (val) {
-                    setState(() {
-                      _showChart = val;
-                    });
-                  },
-                )
-              ],
-            );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text("Show Chart"),
+        Switch(
+          value: _showChart,
+          onChanged: (val) {
+            setState(() {
+              _showChart = val;
+            });
+          },
+        )
+      ],
+    );
   }
 
   final titleController = TextEditingController();
@@ -119,31 +136,29 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
     final txListWidget = Container(
-                    height: (mediaQuery.size.height -
-                            appBar.preferredSize.height -
-                            mediaQuery.padding.top) *
-                        0.7,
-                    child: TransactionList(transactions, _deleteTransaction));
+        height: (mediaQuery.size.height -
+                appBar.preferredSize.height -
+                mediaQuery.padding.top) *
+            0.7,
+        child: TransactionList(transactions, _deleteTransaction));
     final txChartWidget = Container(
-                    height: (mediaQuery.size.height -
-                            appBar.preferredSize.height -
-                            mediaQuery.padding.top) *
-                        (isLandscape ? 0.7 : 0.3),
-                    child: Chart(_recentTransactions));
+        height: (mediaQuery.size.height -
+                appBar.preferredSize.height -
+                mediaQuery.padding.top) *
+            (isLandscape ? 0.7 : 0.3),
+        child: Chart(_recentTransactions));
     return Scaffold(
       appBar: appBar,
+      drawer: AppDrawer(),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            if(isLandscape) _buildLandscapeContent(),
+            if (isLandscape) _buildLandscapeContent(),
             if (!isLandscape) txChartWidget,
             if (!isLandscape) txListWidget,
-            if(isLandscape)
-            _showChart
-                ? txChartWidget
-                : txListWidget
+            if (isLandscape) _showChart ? txChartWidget : txListWidget
           ],
         ),
       ),
